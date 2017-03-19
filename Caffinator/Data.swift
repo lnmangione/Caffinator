b96d499211e9bd19fb6702f5beef13ec66071515
Mikael <mantism@seas.upenn.edu>
2017-03-19 17:39:35 -0400

    adds data.swift

---
+++ Caffinator/Data.swift
@@ -0,0 +1,18 @@
+//
+//  Data.swift
+//  Caffinator
+//
+//  Created by Mikael Mantis on 3/19/17.
+//  Copyright © 2017 AppRoar Studios. All rights reserved.
+//
+
+import Foundation
+
+class Data {
+    
+    static var locationData = (Location)[]
+    
+    static func load() {
+        
+    }
+}
